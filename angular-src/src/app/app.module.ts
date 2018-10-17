f1e00356ad37b1a3b2bd8cb4fd99f32b2cf3dbe7
Adam Jaks <adamjaks97@gmail.com>
2018-10-17 15:44:36 +0200

    base angular and backend connection

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FeedComponent } from './components/feed/feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NavbarComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
