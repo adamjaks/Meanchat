32ca7619c0f3f147c555c22e4ea3c7b7d9d1e10a
Adam Jaks <adamjaks97@gmail.com>
2018-10-18 15:12:16 +0200

    front base progress

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,25 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {FeedComponent} from './components/feed/feed.component';
+import {UserProfileComponent} from './components/user-profile/user-profile.component';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FeedComponent } from './components/feed/feed.component';
+const appRoutes: Routes = [
+    {path: '', component: FeedComponent},
+    {path: 'profile', component: UserProfileComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    NavbarComponent,
-    FeedComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        NavbarComponent,
+        FeedComponent,
+        UserProfileComponent,
+        UserProfileComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+],
+providers: [],
+    bootstrap
+:
+[AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
